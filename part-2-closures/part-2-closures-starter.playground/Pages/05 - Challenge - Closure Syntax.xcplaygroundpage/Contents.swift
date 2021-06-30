3f52b22cd6218afe5c86439f9482890f264efc3f
Student <student@Empowrs-MacBook-Pro.local>
2021-06-30 16:28:53 -0400

    closure syntax

--- part-2-closures/part-2-closures-starter.playground/Pages/05 - Challenge - Closure Syntax.xcplaygroundpage/Contents.swift
+++ part-2-closures/part-2-closures-starter.playground/Pages/05 - Challenge - Closure Syntax.xcplaygroundpage/Contents.swift
@@ -16,10 +16,18 @@ let copyLines = { (offense: String, repeatCount: Int) -> Void in
 }
 // --------------------------------------
 
-// TODO: Write solution here
+typealias Type = (String, Int) -> Void
 
-
-
-
-
+let copyLinesShort: Type = { offense, repeatCount -> Void in
+    print( String(repeating: "I must not \(offense).", count: repeatCount) )
+}
+let copyLinesShortAgain: Type = { offense, repeatCount in
+    print( String(repeating: "I must not \(offense).", count: repeatCount) )
+}
+let copyLinesShortAgainAndAgain: Type = {
+    print( String(repeating: "I must not \($0).", count: $1) )
+}
+copyLinesShort("Cool", 2)
+copyLinesShortAgain("Sweet", 3)
+copyLinesShortAgainAndAgain("Awesome", 4)
 //: [⇒ Next: 06 - forEach and map](@next)
